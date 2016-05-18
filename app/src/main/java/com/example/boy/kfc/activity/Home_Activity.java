36ebbd05ba95ecfca3f309042f5aa18c1a1f145e
“王程远 <mr.wang.cheng.yuan@gmail.com>
2016-05-18 20:17:53 +0800

    BASEUI

---
+++ app/src/main/java/com/example/boy/kfc/activity/Home_Activity.java
@@ -0,0 +1,21 @@
+package com.example.boy.kfc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.boy.kfc.R;
+
+public class Home_Activity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
